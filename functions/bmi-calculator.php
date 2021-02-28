5ecade609577def74cfa9f0df4d4fa6015773f91
makiomar <mo7amed.maki@gmail.com>
2021-02-28 11:02:40 +0200

    Enqueue script within the shortcode

--- functions/bmi-calculator.php
+++ functions/bmi-calculator.php
@@ -3,6 +3,8 @@ if ( !defined('ABSPATH') ) exit();
 
 add_shortcode('anony-bmi-calculator', function($atts){
 	
+	wp_enqueue_script('abmi_bmi');
+	
 	ob_start();
 	include(ABMI_DIR . 'templates/form.php');
 	return ob_get_clean();
